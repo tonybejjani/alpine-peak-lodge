886fe76a4243703fc028b8d782aa7a48f57a54b0
Tony <bejjani.tony@gmail.com>
2024-05-31 06:26:02 +1000

    add toasters: notifications to cabins feature actions

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -5,6 +5,7 @@ import { formatCurrency } from '../../utils/helpers';
 
 import { deleteCabin } from '../../services/apiCabins';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
+import toast from 'react-hot-toast';
 
 const TableRow = styled.div`
   display: grid;
@@ -60,12 +61,12 @@ function CabinRow({ cabin }) {
   const { isLoading: isDeleting, mutate } = useMutation({
     mutationFn: deleteCabin,
     onSuccess: () => {
-      alert('Cabin successfully deleted.');
+      toast.success('Cabin successfully deleted.');
       queryClient.invalidateQueries({
         queryKey: ['cabins'],
       });
     },
-    onError: (err) => alert(err.message),
+    onError: (err) => toast.error(err.message),
   });
 
   return (
