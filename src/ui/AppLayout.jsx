0f3623df17f9def0ad69ef4bec931a2c6a0063e1
Tony <bejjani.tony@gmail.com>
2024-06-05 12:42:05 +1000

    Fix layout to scroll inside main component in appLayout

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -13,9 +13,9 @@ const StyledAppLayout = styled.div`
 `;
 
 const Main = styled.main`
-  background-color: green;
   padding: 4rem 4.8rem 6.4rem;
   background-color: var(--color-grey-50);
+  overflow: scroll;
 `;
 
 const Container = styled.div`
