02ef8093247134584a888b1775c7bc1d6c289105
Tony <bejjani.tony@gmail.com>
2024-07-18 21:42:39 +1000

    add the <BookingTableOperations /> component to modify the filter props in the <Bookings> feature.

--- src/pages/Bookings.jsx
+++ src/pages/Bookings.jsx
@@ -3,11 +3,13 @@
 import Heading from '../ui/Heading';
 import Row from '../ui/Row';
 import BookingTable from '../features/bookings/BookingTable';
+import BookingTableOperations from '../features/bookings/BookingTableOperations';
 
 function Bookings() {
   return (
     <Row type="vertical">
       <Heading as="h1">All bookings</Heading>
+      <BookingTableOperations />
       <BookingTable />
     </Row>
   );
